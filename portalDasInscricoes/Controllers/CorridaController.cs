551920a5e3dfbe9719be03cd40797d2bbe38930d
jacksonbs <jackson.borges@detran.ba.gov.br>
2025-08-03 13:17:21 -0300

    first commit

---
+++ portalDasInscricoes/Controllers/CorridaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portalDasInscricoes.Controllers
+{
+    public class CorridaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ portalDasInscricoes/Repository/CorridaRepository.cs
@@ -0,0 +1,19 @@
+﻿using portalDasInscricoes.Models;
+
+namespace portalDasInscricoes.Repository
+{
+    public static class CorridaRepository
+    {
+        private static List<CorridaModel> _corridas = new List<CorridaModel>();
+        private static int _nextId = 1;
+
+        public static List<CorridaModel> Listar() => _corridas;
+
+        public static void Adicionar(CorridaModel corrida)
+        {
+            corrida.Id = _nextId++;
+            _corridas.Add(corrida);
+        }
+    }
+
+}
