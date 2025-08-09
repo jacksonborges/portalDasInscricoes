e18d72b0576624b3c6a768e8ae33dcd866b74520
jacksonbs <jackson.borges@detran.ba.gov.br>
2025-08-09 16:41:50 -0300

    [FEATURE] Implementando os models.

---
+++ portalDasInscricoes/Models/CategoriaModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalInscricoes.Models
+{
+    public class CategoriaModel
+    {
+        public int Id { get; set; }
+
+        // FK Evento
+        public int EventoId { get; set; }
+        public EventoModel Evento { get; set; }
+
+        [Required, StringLength(100)]
+        public string NomeCategoria { get; set; } // Ex.: "5K", "10K"
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        public int? LimiteParticipantes { get; set; }
+
+        // Relacionamentos
+        public ICollection<InscricaoModel> Inscricoes { get; set; }
+    }
+}
---
+++ portalDasInscricoes/Models/EventoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PortalInscricoes.Models
+{
+    public class EventoModel
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(150)]
+        public string Nome { get; set; }
+
+        [Required]
+        public DateTime DataEvento { get; set; }
+
+        [Required, StringLength(200)]
+        public string Local { get; set; }
+
+        [Required, StringLength(2)]
+        public string Estado { get; set; }
+
+        [Required, StringLength(100)]
+        public string Cidade { get; set; }
+
+        [StringLength(200)]
+        public string Endereco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        [Required]
+        public DateTime DataInicioInscricao { get; set; }
+
+        [Required]
+        public DateTime DataFimInscricao { get; set; }
+
+        [StringLength(20)]
+        public string Situacao { get; set; } = "Aberta";
+
+        public string Regulamento { get; set; }
+
+        // FK Organizador
+        public int OrganizadorId { get; set; }
+        public UsuarioModel Organizador { get; set; }
+
+        // Relacionamentos
+        public ICollection<CategoriaModel> Categorias { get; set; }
+        public ICollection<InscricaoModel> Inscricoes { get; set; }
+    }
+}
