4112d8a3df010ced775578d00e9bf064abac4cf4
jacksonbs <jackson.borges@detran.ba.gov.br>
2025-08-11 09:44:51 -0300

    [FIX] Organizando extrutura.

---
+++ portalDasInscricoes/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortalDasInscricoes.Business.Util;
+using PortalDasInscricoes.Repository.Context;
+
+namespace portalDasInscricoes
+{
+    public static class DependencyInjection
+    {
+        public static WebApplicationBuilder ConfigureDI(this WebApplicationBuilder builder)
+        {
+            Settings.IS_DESENV = builder.Configuration["Ambiente"] == "2";
+
+            builder.Services.AddDbContext<DbPortalDasIncricoesContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("PortalDasInscricoesConnectionString")));
+           
+            return builder;
+        }
+
+        public static async Task<WebApplication> ConfigureSettingsAsync(this WebApplication webApplication)
+        {
+            using (var serviceScope = webApplication.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+            }
+
+            return webApplication;
+        }
+    }
+}
+
