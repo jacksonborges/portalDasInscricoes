068b54ca96950fcb9f621a5bd7e04d128d21a613
jacksonbs <jackson.borges@detran.ba.gov.br>
2025-08-12 09:24:59 -0300

    [FIX] Implementando as classes Entities.

--- PortalDasInscricoes.Repository/Entities/Categoria.cs
+++ PortalDasInscricoes.Repository/Entities/Categoria.cs
@@ -1,6 +1,24 @@
-﻿namespace PortalDasInscricoes.Repository.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortalDasInscricoes.Repository.Entities
 {
     public class Categoria
-    {
+    {        
+        public int Id { get; set; }
+        // FK Evento
+        public int EventoId { get; set; }
+        //public Evento Evento { get; set; }
+
+        [Required, StringLength(100)]
+        public string NomeCategoria { get; set; } // Ex.: "5K", "10K"
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        public int? LimiteParticipantes { get; set; }
+
+        // Relacionamentos
+        //public ICollection<Inscricao> Inscricoes { get; set; }
+
     }
 }
---
+++ PortalDasInscricoes.Repository/Entities/Pagamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDasInscricoes.Repository.Entities
+{
+    internal class Pagamento
+    {
+        public int Id { get; set; }
+
+        // FK Inscricao
+        public int InscricaoId { get; set; }
+        public Inscricao Inscricoes { get; set; }
+
+        [Required]
+        public decimal Valor { get; set; }
+
+        [Required, StringLength(50)]
+        public string Metodo { get; set; } // Cartão, PIX, Boleto
+
+        public DateTime? DataPagamento { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = "Pendente";
+
+        public string TransacaoId { get; set; }
+    }
+}
